50a35b385ed6043d9b8c69fcf13a828b048f17fe
Aivar Annamaa <aivar.annamaa@gmail.com>
2016-08-22 15:40:37 +0300

    pngmath => imgmath

--- source/conf.py
+++ source/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.pngmath']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.imgmath']
 todo_include_todos = False
 
 pngmath_dvipng_args = ['-gamma 1.5', '-D 110', '-bg', 'Transparent']
